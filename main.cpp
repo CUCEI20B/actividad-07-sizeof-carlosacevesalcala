5c92fe0094e19d1e1964749f09896325a29a3909
Carlos Alcala <carlos.aceves2888@alumnos.udg.mx>
2020-10-05 18:26:36 -0500

    bytes de tipo de datos

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
+#include <float.h>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    int numero;
+    string cadena;
 
+    cin>>cadena;
+
+    if (cadena == "char"){
+        numero=CHAR_BIT;
+    }
+    if (cadena == "int"){
+        numero=__SIZEOF_INT__;
+    }
+    if (cadena == "short"){
+        numero=__SIZEOF_SHORT__;
+    }
+    if (cadena == "float"){
+        numero=__SIZEOF_FLOAT__;
+    }
+    if (cadena == "double"){
+        numero=__SIZEOF_DOUBLE__;
+    }
+    if (cadena == "long"){
+        numero=__SIZEOF_LONG__;
+    }
+
+    cout<<numero;
     return 0;
 }
